b01e8a7ecf2841f0248bd9fb507d3409be79a728
KnifeSGS <knife.sgs@gmail.com>
2020-11-21 12:41:36 +0100

    lufipukkasztó kész

--- gyakorlo_feladatok/js/balloon.js
+++ gyakorlo_feladatok/js/balloon.js
@@ -1,32 +1,38 @@
 'use strict';
 
+const colors = document.querySelectorAll('.balloon__colors');
+const win = document.querySelector('.balloon__win');
+const balloons = document.querySelectorAll('.balloon__buttons')
 let popCounter = 0;
+
 (function popBalloon() {
-    const colors = document.querySelectorAll('.balloon__colors');
-    const win = document.querySelector('.balloon__win');
-    const balloons = document.querySelectorAll('.balloon__buttons')
+    mouseOver();
+    mouseLeave();
+}) ();
+
+function mouseOver() {
     for (let i = 0; i < colors.length; i += 1) {
         colors[i].addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = 'transparent';
-            event.target.style.fontSize = '1rem';
-            popCounter += 1;
-            if (popCounter >= 25) {
-                for (let k = 0; k < balloons.length; k += 1) {
-                    balloons[k].style.display = 'none';
-                }
-                win.style.display = 'flex';
-            }
-
-        })
-    };
-    
-    for (let j = 0; j < colors.length; j += 1) {
-        colors[j].addEventListener('mouseleave', (event) => {
-            event.target.style.fontSize = '0';
-            event.target.removeEventListener('mouseover', (event) => {
-                event.target.style.backgroundColor = 'transparent';
-                event.target.style.fontSize = '1rem';
-            });
+            event.target.classList.add('balloon--popTarget');
+            countingPops();
         })
     }
-})();
+};
+
+function countingPops() {
+    popCounter += 1;
+    if (popCounter >= 25) {
+        for (let k = 0; k < balloons.length; k += 1) {
+            balloons[k].classList.add('disappear');
+        }
+            win.classList.add('emerge');
+    }
+};
+
+function mouseLeave() {
+    for (let j = 0; j < colors.length; j += 1) {
+        colors[j].addEventListener('mouseleave', (event) => {
+            event.target.classList.add('disappear');
+        })
+    }
+};
