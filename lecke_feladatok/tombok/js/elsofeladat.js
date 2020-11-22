5e88a82b1899a6509f507d0cab775d577ec29c8e
KnifeSGS <knife.sgs@gmail.com>
2020-11-22 17:15:38 +0100

    első kész

---
+++ lecke_feladatok/tombok/js/elsofeladat.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Írj egy olyan függvény kifejezést, ami paraméterként vár egy egész számokat 
+// tartalmazó tömböt!(Input ellenőrzést nem kell végezni.)
+// A függvény szorozza meg tömbelemek értékét 1.27 - el, majd pedig térjen 
+// vissza az elemek összegével!
+
+const egeszSzamosTomb = [123, 456, 789] 
+
+const afaOsszesito = arr => {
+    const sumAfasTomb = arr
+    .map(item => item * 1.27)
+    .reduce((previousValue, currentValue) => previousValue + currentValue);
+    return sumAfasTomb;
+}
+
+// const sumAfasTomb = egeszSzamosTomb
+
+// console.log(sumAfasTomb);
+console.log(afaOsszesito(egeszSzamosTomb));
+
