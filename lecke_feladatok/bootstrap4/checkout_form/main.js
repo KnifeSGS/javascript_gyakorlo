fa3754ea9340826423bea77622df3c4433871edb
KnifeSGS <knife.sgs@gmail.com>
2021-01-08 17:52:01 +0100

    bootstrap4 checkout form html kész js kiválasztás nem működik

--- lecke_feladatok/bootstrap4/checkout_form/main.js
+++ lecke_feladatok/bootstrap4/checkout_form/main.js
@@ -1,87 +1,106 @@
 'use strict';
 
-const statesHungary = [
-    'Bács-Kiskun',
-    'Baranya',
-    'Békés',
-    'Borsod-Abaúj-Zemplén',
-    'Csongrád-Csanád',
-    'Fejér',
-    'Győr-Moson-Sopron',
-    'Hajdú-Bihar',
-    'Heves',
-    'Jász-Nagykun-Szolnok',
-    'Komárom-Esztergom',
-    'Nógrád',
-    'Pest',
-    'Somogy',
-    'Szabolcs-Szatmár-Bereg',
-    'Tolna',
-    'Vas',
-    'Veszprém',
-    'Zala',
-    'Budapest'
-];
-
-const statesUSA = [
-    'Alabama',
-    'Alaszka',
-    'Arizona',
-    'Arkansas',
-    'Colorado',
-    'Connecticut',
-    'Delaware',
-    'Dél-Dakota',
-    'Dél-Karolina',
-    'Észak-Dakota',
-    'Észak-Karolina',
-    'Florida',
-    'Georgia',
-    'Hawaii',
-    'Idaho',
-    'Illinois',
-    'Indiana',
-    'Iowa',
-    'Kalifornia',
-    'Kansas',
-    'Kentucky',
-    'Louisiana',
-    'Maine',
-    'Maryland',
-    'Massachusetts',
-    'Michigan',
-    'Minnesota',
-    'Mississippi',
-    'Missouri',
-    'Montana',
-    'Nebraska',
-    'Nevada',
-    'New Hampshire',
-    'New Jersey',
-    'New York',
-    'Nyugat-Virginia',
-    'Ohio',
-    'Oklahoma',
-    'Oregon',
-    'Pennsylvania',
-    'Rhode Island',
-    'Tennessee',
-    'Texas',
-    'Új-Mexikó',
-    'Utah',
-    'Vermont',
-    'Virginia',
-    'Washington',
-    'Wisconsin',
-    'Wyoming'
-];
+const countryStates = {
+    'Hungary': [
+        'Bács-Kiskun',
+        'Baranya',
+        'Békés',
+        'Borsod-Abaúj-Zemplén',
+        'Csongrád-Csanád',
+        'Fejér',
+        'Győr-Moson-Sopron',
+        'Hajdú-Bihar',
+        'Heves',
+        'Jász-Nagykun-Szolnok',
+        'Komárom-Esztergom',
+        'Nógrád',
+        'Pest',
+        'Somogy',
+        'Szabolcs-Szatmár-Bereg',
+        'Tolna',
+        'Vas',
+        'Veszprém',
+        'Zala',
+        'Budapest'
+    ],
+    'USA': [
+        'Alabama',
+        'Alaszka',
+        'Arizona',
+        'Arkansas',
+        'Colorado',
+        'Connecticut',
+        'Delaware',
+        'Dél-Dakota',
+        'Dél-Karolina',
+        'Észak-Dakota',
+        'Észak-Karolina',
+        'Florida',
+        'Georgia',
+        'Hawaii',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Iowa',
+        'Kalifornia',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Maine',
+        'Maryland',
+        'Massachusetts',
+        'Michigan',
+        'Minnesota',
+        'Mississippi',
+        'Missouri',
+        'Montana',
+        'Nebraska',
+        'Nevada',
+        'New Hampshire',
+        'New Jersey',
+        'New York',
+        'Nyugat-Virginia',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Rhode Island',
+        'Tennessee',
+        'Texas',
+        'Új-Mexikó',
+        'Utah',
+        'Vermont',
+        'Virginia',
+        'Washington',
+        'Wisconsin',
+        'Wyoming'
+    ]
+}
 
 const country = document.querySelector('#country');
 const state = document.querySelector('#state');
 
-(function chooseState () {
-    if (country.value == 'Hungary') {
-        state.value = statesHungary
+function* chooseState(selectedCountry) {
+    for (const item of countryStates[selectedCountry]) {
+        yield item;
     }
+};
+
+const setState = (selectedCountry) => {
+        for (let i = 0; i < countryStates[selectedCountry].length; i += 1) {
+            console.log(countryStates[selectedCountry][i]);
+            let option = document.createElement('option');
+            option.textContent = countryStates[selectedCountry][i];
+            option.value = countryStates[selectedCountry][i];
+            state.add(option)
+        }
+    
+};
+
+const eventHandler = () => {
+    state.addEventListener('click', setState())
 }
-)();
+
+// const items = chooseState("Hungary");
+// console.log(items.next().value);
+setState('Hungary')
